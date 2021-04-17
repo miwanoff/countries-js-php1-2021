9f7c119f8af2f3c5ca2b58698ed250c144940d32
miwanoff <marina.iva.noffa@gmail.com>
2021-04-17 14:22:24 +0300

    add $_SERVER['PHP_SELF']

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 include "db.php";
 include "header.php";
-$user_form = '<form action="index.php" method="post" name="autoForm">
+$user_form = '<form action="' . $_SERVER['PHP_SELF'] . '" method="post" name="autoForm">
 Логин: <input type="text" name="login">
 Пароль: <input type="password" name="pass">
 <input type="submit" value="Подтвердить" name="go">
